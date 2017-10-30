de1c0c86a529189602631f605f82540decafa3cc
Manuel Colmenero Navarro <manuelcolmenero@gmail.com>
2017-10-30 23:47:15 +0100

    actualización de pantalla main

--- app/src/main/java/com/mcolmenero/guedr/MainActivity.kt
+++ app/src/main/java/com/mcolmenero/guedr/MainActivity.kt
@@ -2,11 +2,15 @@ package com.mcolmenero.guedr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = MainActivity::class.java.canonicalName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.v(TAG, "He pasado por onCreate")
     }
 }
